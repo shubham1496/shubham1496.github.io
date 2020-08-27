64f716cfdc1e6c402f5e2e7230cc150613bdfdb4
shubham1496 <44375557+shubham1496@users.noreply.github.com>
2020-08-27 23:50:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+  }
+}
+
+
+
 var actual_amount = 0;
 var interest = 0;
 var years = 0;
@@ -34,7 +66,7 @@ document.addEventListener('DOMContentLoaded', () => {
     var total_interest = interest_amt * years
     var total_outstanding = total_interest + actual_amount + incidential_charges
     var instalment = (total_outstanding / (years * 12))
-    document.querySelector('#instalment_display').textContent = `Instalment = ${total_outstanding}/${years * 12}`
+    document.querySelector('#instalment_display').textContent = `(${actual_amount} + ${interest_amt} + ${incidential_charges}) / ${years * 12}`
     document.querySelector('#result').innerHTML = `<h5>${instalment}</h5>`
     return false
   }
